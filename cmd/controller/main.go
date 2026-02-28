50d941b0ef49daf514bc6d48972e680f7c7bf695
Ynez Dias <ynezdias0501@gmail.com>
2026-02-28 12:27:58 -0500

    controller main edited

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -2,14 +2,66 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"sync"
 	"time"
 )
 
-func main() {
-	fmt.Println("[Controller] Starting heartbeat monitor...")
+type NodeInfo struct {
+	LastSeen time.Time
+	Status   string
+}
 
-	ticker := time.NewTicker(5 * time.Second)
-	for range ticker.C {
-		fmt.Printf("[Controller] Checking heartbeats at %s\n", time.Now().Format(time.RFC3339))
+var (
+	nodes   = make(map[string]*NodeInfo)
+	mu      sync.Mutex
+	timeout = 5 * time.Second
+)
+
+func heartbeatHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	if _, exists := nodes[id]; !exists {
+		nodes[id] = &NodeInfo{}
+	}
+
+	nodes[id].LastSeen = time.Now()
+	nodes[id].Status = "Alive"
+
+	fmt.Printf("[Controller] Heartbeat received from Node-%s\n", id)
+}
+
+func monitorFailures() {
+	for {
+		time.Sleep(2 * time.Second)
+
+		mu.Lock()
+		for id, info := range nodes {
+			if time.Since(info.LastSeen) > timeout {
+				if info.Status != "Suspected" {
+					info.Status = "Suspected"
+					fmt.Printf("[Controller] Node-%s suspected FAILED\n", id)
+				}
+			}
+		}
+		mu.Unlock()
+	}
+}
+
+func main() {
+	fmt.Println("[Controller] Starting...")
+
+	http.HandleFunc("/heartbeat", heartbeatHandler)
+
+	go monitorFailures()
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
