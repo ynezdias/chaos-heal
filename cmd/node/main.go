6265d6f4b73dc5393ba0aa808c09160b6acc9270
Ynez Dias <ynezdias0501@gmail.com>
2026-02-28 12:27:36 -0500

    node main edited

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -3,20 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
 )
 
 func main() {
 	nodeID := os.Getenv("NODE_ID")
-	if nodeID == "" {
-		log.Fatal("NODE_ID not set")
+	controllerURL := os.Getenv("CONTROLLER_URL")
+
+	if nodeID == "" || controllerURL == "" {
+		log.Fatal("NODE_ID or CONTROLLER_URL not set")
 	}
 
 	fmt.Printf("[Node-%s] Starting...\n", nodeID)
 
 	ticker := time.NewTicker(2 * time.Second)
+
 	for range ticker.C {
-		fmt.Printf("[Node-%s] Heartbeat sent at %s\n", nodeID, time.Now().Format(time.RFC3339))
+		url := fmt.Sprintf("%s/heartbeat?id=%s", controllerURL, nodeID)
+		_, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("[Node-%s] Failed to send heartbeat: %v\n", nodeID, err)
+			continue
+		}
+		fmt.Printf("[Node-%s] Heartbeat sent\n", nodeID)
 	}
 }
